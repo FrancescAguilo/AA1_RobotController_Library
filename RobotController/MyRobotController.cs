829944a806c32fded9c07152d4ee0bc3a30a6070
Victor Blas <victor.blas@enti.cat>
2020-11-02 19:34:01 +0100

    gitflow-feature-stash: OnlyTesting_NeverMerge

--- RobotController/MyRobotController.cs
+++ RobotController/MyRobotController.cs
@@ -38,7 +38,7 @@ namespace RobotController
         public string Hi()
         {
 
-            string s = "hello world from my Robot Controller";
+            string s = "hello world from my ENTI jhkkftfktyukgf";
             return s;
 
         }
@@ -53,6 +53,7 @@ namespace RobotController
             rot1 = NullQ;
             rot2 = NullQ;
             rot3 = NullQ;
+
         }
 
 
@@ -163,7 +164,14 @@ namespace RobotController
         internal MyQuat Multiply(MyQuat q1, MyQuat q2) {
 
             //todo: change this so it returns a multiplication:
-            return NullQ;
+            MyQuat returnQuat = NullQ;
+
+            returnQuat.x = q1.x * q2.w + q1.y * q2.z - q1.z * q2.y + q1.w * q2.x;
+            returnQuat.y = -q1.x * q2.z + q1.y * q2.w + q1.z * q2.x + q1.w * q2.y;
+            returnQuat.z = q1.x * q2.y - q1.y * q2.x + q1.z * q2.w + q1.w * q2.z;
+            returnQuat.w = -q1.x * q2.x - q1.y * q2.y - q1.z * q2.z + q1.w * q2.w;
+
+            return returnQuat;
 
         }
 
@@ -171,15 +179,19 @@ namespace RobotController
         {
 
             //todo: change this so it takes currentRotation, and calculate a new quaternion rotated by an angle "angle" radians along the normalized axis "axis"
+
             return NullQ;
 
         }
 
-
+        internal MyVec 
 
 
         //todo: add here all the functions needed
-
+        internal void ForwardKinematics(MyQuat[] _joints, float[] _angles)
+        {
+            MyVec prevPoint = 
+        }
         #endregion
 
 
